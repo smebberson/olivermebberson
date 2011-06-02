18a19e7549ccd86f37a3770a118704f82df20097
Scott Mebberson <scott@scottmebberson.com>
2011-06-02 13:46:38 +0930

    Added bootstrap code to make testing easier

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,31 @@
+import grails.util.Environment
+import com.olivermebberson.Saying
+
 class BootStrap {
 
     def init = { servletContext ->
+		
+		switch (Environment.current) {
+			
+			case Environment.DEVELOPMENT:
+				new Saying(
+					title: 'Published and public',
+					description: 'This is the description for the saying',
+					status: 'published',
+					roles: 'public',
+					saidOn: new Date("1/1/2010")
+				).save(true);
+				new Saying(
+					title: 'Published and public',
+					description: 'This is the description for the saying number 2',
+					status: 'published',
+					roles: 'public',
+					saidOn: new Date("1/1/2011")
+				).save(true);
+			break;
+			
+		}
+		
     }
     def destroy = {
     }
