f01b6a51931eefca0c8860e6d8086bf231abc3e7
Scott Mebberson <scott@scottmebberson.com>
2011-06-06 23:36:33 +0930

    Updated the bootstrap data, to test wrapping and formatting more easily

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -10,17 +10,17 @@ class BootStrap {
 			case Environment.DEVELOPMENT:
 				new Saying(
 					title: 'Published and public',
-					description: 'This is the description for the saying',
+					description: 'This is the description for the saying. I\'m going to write something fairly lengthy in here because I want to make sure it all wrapes nicely.',
 					status: 'published',
 					roles: 'public',
-					saidOn: new Date("1/1/2010")
+					saidOn: new Date("2010/12/1")
 				).save(true);
 				new Saying(
 					title: 'Published and public',
 					description: 'This is the description for the saying number 2',
 					status: 'published',
 					roles: 'public',
-					saidOn: new Date("1/1/2011")
+					saidOn: new Date("2011/5/31")
 				).save(true);
 			break;
 			
