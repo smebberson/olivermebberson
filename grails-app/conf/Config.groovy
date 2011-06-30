9580310aaa82b5f16e1dbda1d285281ccd1d9c02
Scott Mebberson <scott@scottmebberson.com>
2011-06-30 11:43:49 +0930

    Installed the grails flickr plugin (http://www.grails.org/plugin/flickr)

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -10,6 +10,8 @@
 //    grails.config.locations << "file:" + System.properties["${appName}.config.location"]
 // }
 
+grails.plugins.flickr.apiKey = "a2d1364931efd2673a54b0a55accd6d4"
+
 grails.project.groupId = appName // change this to alter the default package name and Maven publishing destination
 grails.mime.file.extensions = true // enables the parsing of file extensions from URLs into the request format
 grails.mime.use.accept.header = false
